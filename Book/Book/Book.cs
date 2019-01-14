1b604071c9bdf3335bfb0c643461b4d906d8345d
Marko Saarainen <marko.s.saarainen@gmail.com>
2019-01-14 09:33:00 +0200

    initialized Book program and done

---
+++ Book/Book/Book.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kirjaohjelma
+{
+    class Book
+    {
+        public string Title;
+        public string Author;
+        public int Id;
+        public double Price;
+
+        public Book()
+        {
+            Title = "Turmoil";
+            Author = "Agatha.G Fuller";
+            Id = 1;
+            Price = 9.90;
+        }
+
+        public Book(string title, string author, int id, double price)
+        {
+            Title = title;
+            Author = author;
+            Id = id;
+            Price = price;
+        }
+
+        public void printBook()
+        {
+            Console.WriteLine($"Title: {Title}\nAuthor: {Author}\nId: {Id}\nPrice: {Price:c}");
+        }
+
+        public string CompareBook(Book book)
+        {
+            if (this.Price > book.Price)
+            {
+                return $"{this.Title} on kalliimpi {book.Title}.";
+            }
+            else if (this.Price < book.Price)
+            {
+                return $"{this.Title} on Halvempi {book.Title}.";
+            }
+            else
+            {
+                return "Kirjat on saman hintaisia";
+            }
+        }
+    }
+}
---
+++ Book/Book/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace kirjaohjelma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            CultureInfo.CurrentCulture = new CultureInfo("fi-FI");
+            Book book = new Book();
+
+            book.printBook();
+
+            Book newBook = new Book("Cenocide", "M.C Wensforth", 2, 19.90);
+
+            newBook.printBook();
+
+            Console.WriteLine(newBook.CompareBook(book));
+
+            Console.ReadKey();
+        }
+    }
+}
