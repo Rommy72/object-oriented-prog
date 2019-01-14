865fe52571f5e8882aec7692da86109ba62b85ce
Marko Saarainen <marko.s.saarainen@gmail.com>
2019-01-14 09:18:30 +0200

    Cat initialized and done

---
+++ Cat/Cat/Cat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cat
+{
+    
+    class Cat
+    {
+        public string name;
+        public int weight;
+
+        public Cat(string name, int weightvalue)
+        {
+            this.name = name;
+            this.weight = 1;
+            Console.WriteLine($"kissan paino on {this.weight}");
+        }
+
+        public Cat(int weightValue)
+        {
+            this.weight = weightValue;
+            // Console.WriteLine($"Kissan paino{this.weight}");
+        }
+        public void IncreaseWeight(int value)
+        {
+            this.weight += value;
+
+            Console.WriteLine($"Kissan{this.name} paino on {this.weight} ");
+        }
+        public void PrintWeight()
+        {
+            Console.WriteLine($"Kissan {this.name} paino on {this.weight}");
+        }
+
+    }
+}
+
---
+++ Cat/Cat/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cat
+{
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Tämä on kissaohjelma ... ");
+
+                Cat my1StCat = new Cat(" Pilli", 1);
+                my1StCat.IncreaseWeight(1);
+                Cat my2DNcat = new Cat(" Pulla", 3);
+                my2DNcat.PrintWeight();
+
+                Console.Write("Painahan mitä vaan näppylää...\n");
+                Console.ReadKey();
+
+            }
+        }
+    }
+
+
+    
+
